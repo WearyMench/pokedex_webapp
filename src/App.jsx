68280ea871aa53666c267bcdffb4798f6bc007ed
Adrian07 <adrian.mirabal07@gmail.com>
2022-12-15 10:54:54 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src="./src/assets/mew.gif" alt="pokemon gif" />
+    </div>
+  );
+}
+
+export default App;
