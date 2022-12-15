fffecb0e0c2f34f22f6b5a6882baa1a3604049f8
Adrian07 <adrian.mirabal07@gmail.com>
2022-12-15 11:37:49 -0400

    little change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <img src="./src/assets/mew.gif" alt="pokemon gif" />
+      <img src="./src/assets/Pokebola.png" alt="pokemon gif" />
     </div>
   );
 }
