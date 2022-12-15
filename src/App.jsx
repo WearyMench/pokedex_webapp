7bb46ad777ba66ed15120afc07b9853ec647bd15
Adrian07 <adrian.mirabal07@gmail.com>
2022-12-15 12:39:57 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <img src="../mew.gif" alt="pokegif" />
+      <img src="src/assets/mew.gif" alt="pokegif" />
     </div>
   );
 }
