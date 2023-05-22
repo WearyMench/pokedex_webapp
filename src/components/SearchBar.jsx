e9acce9b2158f5c1de265652721060215ac05549
Adrian Mirabal <adrian.mirabal07@gmail.com>
2023-05-21 23:24:21 -0400

    Search by Enter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,14 +11,25 @@ function SearchBar({ onSearch }) {
     }
   };
 
-  const onClick = async (e) => {
-    onSearch(search);
+  const onClick = () => {
+    onSearch(search.toLowerCase());
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      onSearch(search.toLowerCase());
+    }
   };
 
   return (
     <div className="searchbar-container">
       <div className="searchbar">
-        <input placeholder="Name or Id of any Pokemon" onChange={onChange} />
+        <input
+          placeholder="Name or Id of any Pokemon"
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
+        />
       </div>
       <div className="searchbar-btn">
         <button onClick={onClick}>
